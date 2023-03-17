28ea95262da18f3463d212536365dde57d8168ae
ELHEND <alhnd683@gmail.com>
2023-03-17 07:58:50 -0700

    x

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Description: 'print comb of numbers'
- * Return: always 0
- */
+  *main - Entry point 
+  *Description: 'print comb of numbers'
+  *Return: always 0
+  */
 int main(void)
 {
 int i;
