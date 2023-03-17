f3fe6489d23da15e40509efe06a99b13f717835a
ELHEND <alhnd683@gmail.com>
2023-03-17 03:54:53 -0700

    x

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ ** main - Entry point. Prints the alphabet in lowercase.
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char letter = 'a';
+	        
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+
+return (0);
+}
+
