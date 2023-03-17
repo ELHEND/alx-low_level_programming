230009b514550b9bc7b536cfaf98c2abd9f5053c
ELHEND <alhnd683@gmail.com>
2023-03-17 03:31:22 -0700

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * This program will assign a random number to the
- *     * variable n each time it is executed. Complete the
- *      * source code in order to print whether the number
- *       * stored in the variable n is positive or negative.
- *        *
- *         * Return: Always 0 (Success)
- *          */
+ ** main - Entry point
+ **
+ ** This program will assign a random number to the
+ ** variable n each time it is executed. Complete the
+ ** source code in order to print whether the number
+ ** stored in the variable n is positive or negative.
+ **
+ ** Return: Always 0 (Success)
+ **/
 
 int main(void)
 { 
