10ab02191459a06aa139f6e035db082cf8498932
ELHEND <alhnd683@gmail.com>
2023-03-17 04:24:22 -0700

    g

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- ** main - Entry point. Prints the alphabet in lowercase.
- **
- ** Return: Always 0 (Success)
- **/
+ *  * main - Entry point. Prints the alphabet in lowercase.
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 char letter = 'a';
