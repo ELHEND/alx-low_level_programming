3b46e884ba3ba924141d73e009509442cbd858c7
ELHEND <alhnd683@gmail.com>
2023-03-17 06:14:23 -0700

    x

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+/**
+ ** main -Entry point
+ ** Description: 'print alphabets lowercase and uppercase'
+ ** Return: always 0
+ **/
 int main(void)
 {
 int i;
-
 for (i = 'a'; i <= 'z'; i++)
 putchar(i);
 for (i = 'A'; i <= 'Z'; i++)
 putchar(i);
 putchar('\n');
-
 return (0);
 }
-
