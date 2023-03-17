75681464748eebc511b5303f604ec16e05533dd0
ELHEND <alhnd683@gmail.com>
2023-03-17 06:37:17 -0700

    x

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print numbers of base 10'
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+	        
+for (i = 0; i < 10; i++)
+printf("%d", i);
+		        
+printf("\n");
+		       
+return 0;
+}
