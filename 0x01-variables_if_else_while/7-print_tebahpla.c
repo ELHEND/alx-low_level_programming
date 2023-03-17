f0ed951952e14aacc182a8afbc74eef55be7698d
ELHEND <alhnd683@gmail.com>
2023-03-17 07:34:51 -0700

    x

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'point alphabets in reverse'
+ * Return: Always 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
