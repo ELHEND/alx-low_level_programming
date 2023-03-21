2196a1a50134355a347976569d96cd9dbecc75d3
ELHEND <alhnd683@gmail.com>
2023-03-21 04:31:35 -0700

    x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 /**
- ** main -Entry point
- **
- ** Return:Always 0 (success)
- **/
-int main(void)
-{
-char c[] = "_putchar\n";
-int i;
-for (i = 0; c[i] != '\0'; i++)
-{
-_putchar(c[i]);
-}
+ * main - Entry point
+ * Description: 'points _putchar to stdout'
+ * Return: always 0
+ */
+int main(void) 
+{ 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
-}
+} 
