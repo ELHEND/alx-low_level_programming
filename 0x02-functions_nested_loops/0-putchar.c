433752d6aeed08177478fef82eb992b345aabd0b
ELHEND <alhnd683@gmail.com>
2023-03-21 04:34:13 -0700

    x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,8 @@
  * Description: 'points _putchar to stdout'
  * Return: always 0
  */
-int main(void) 
-{ 
+int main(void)
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -16,4 +16,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-} 
+}
