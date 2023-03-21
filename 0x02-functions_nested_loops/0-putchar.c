62661c4e5aca5c1cf1d7a90a29a8bea0adc1c35d
ELHEND <alhnd683@gmail.com>
2023-03-21 03:26:46 -0700

    x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
- /**
-  ** main - Entry point
+nclude "holberton.h" 
+ /** 
+  ** main - Entry point 
   ** 
-  ** Return: Always 0 (Success)
-  **/
- int main(void)
-{
-char *sh = "Holberton"; 
-while (*sh)	
-{
-putchar(*sh);
-sh++;
-}
-_putchar('\n');
-return (0);
+  ** Return: Always 0 (Success) 
+  **/ 
+ int main(void) 
+{ 
+char *sh = "Holberton"; 		    
+while (*sh) 
+{ 
+_putchar(*sh); 
+sh++; 
+} 
+_putchar('\n'); 		      
+return (0); 
 }
