4f5f79aedd9a55a5fac053d532fd892c3ef8c623
ELHEND <alhnd683@gmail.com>
2023-03-21 07:25:47 -0700

    x

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,1 +1,11 @@
-hend
+#include <unistd.h> 
+/**
+ * Description : _putchar.c function to print single character.
+ *
+ * c is the character to bassed as aparmeter to be printed
+ *
+ */
+void _putchar(char c) 
+{ 
+write(1, &c, 1);
+} 
