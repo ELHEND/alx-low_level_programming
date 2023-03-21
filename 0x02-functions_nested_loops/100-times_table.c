b1145e771e173367619f0c9e823e3ece68f6595e
ELHEND <alhnd683@gmail.com>
2023-03-21 11:57:39 -0700

    x

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 int i, j, result;
 if (n < 0 || n > 15)
-{ 
+{
 return;
 }
 for (i = 0; i <= n; i++)
@@ -26,9 +26,8 @@ else
 _putchar(' ');
 if (result < 10)
 {
-_putchar(' ');					
 _putchar(' ');
-_putchar(result + '0');	
+_putchar(result + '0');
 }
 else if
 (result < 100)
@@ -45,7 +44,7 @@ _putchar(result % 10 + '0');
 }
 }
 if (j != n)
-{	
+{
 _putchar(',');
 _putchar(' ');
 }
