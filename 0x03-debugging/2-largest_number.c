a2dea95e01bf7b40909a357891c54753afdf426c
ELHEND <alhnd683@gmail.com>
2023-03-22 11:53:53 -0700

    x

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,21 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b)
-{
-if (a > c)
-{
-largest = a;
-}
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+		int largest;
+
+			if (a > b)
+					{
+								if (a > c)
+											{
+															largest = a;
+																	}
+									}
+
+				else if (b > c)
+						{
+									largest = b;
+										}
+					else
+							{
+										largest = c;
+											}
+
+						return (largest);
 }
