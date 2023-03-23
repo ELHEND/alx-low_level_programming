c4a7052a7e7eac5e71c43ef250df1a84cdf8e744
ELHEND <alhnd683@gmail.com>
2023-03-23 07:29:43 -0700

    x

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,15 @@
  */
 void print_line(int n)
 {
-		if (n <= 0)
+if (n <= 0)
 				{
 							_putchar('\n');
-								} else
+}
+else
 										{
 													int i;
 
-															for (i = 1; i <= n; i++)
+for (i = 1; i <= n; i++)
 																		{
 																						_putchar('_');
 																								}
