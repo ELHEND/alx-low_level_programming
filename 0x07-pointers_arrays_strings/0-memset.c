e300bf4ebfd080f979e9fc754f4684f335ae057d
ELHEND <alhnd683@gmail.com>
2023-04-03 03:28:24 -0700

    x

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- **_memset - fills memory with a constant byte
+ *_memset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
