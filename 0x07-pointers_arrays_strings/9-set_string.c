f8e6344e8327214e8235933f8e4a53eac44ba9fd
ELHEND <alhnd683@gmail.com>
2023-04-03 05:46:11 -0700

    x

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *set_string - sets the value of a pointer to a char
+ *
+ *@s: double pointer that will be set to a char
+ *@to: pointer thathas the value of the char
+ *
+ *Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
