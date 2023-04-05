5304679e0cd014f0a9428c67d2a4234e19564567
ELHEND <alhnd683@gmail.com>
2023-04-05 10:32:56 -0700

    x

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,59 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-int _sqrt_recursion(int n);
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
+int actual_sqrt_recursion(int n, int i);
 
-r = _sqrt_recursion(1);
-printf("%d\n", r);
-r = _sqrt_recursion(1024);
-printf("%d\n", r);
-r = _sqrt_recursion(16);
-printf("%d\n", r);
-r = _sqrt_recursion(17);
-printf("%d\n", r);
-r = _sqrt_recursion(25);
-printf("%d\n", r);
-r = _sqrt_recursion(-1);
-printf("%d\n",r);
-return (0);
-}
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ *
  */
-int _sqrt_recursion(int n)
-{
-/* base cases */
-if (n < 0) /* negative input */
-return (-1);
-else if (n == 0 || n == 1) /* square root is itself */
-return (n);
-/*
- * recursive case:
- * we check each number from 1 to n/2,
- * trying to find a number whose square equals n
- */
-int i;
-for (i = 1; i <= n/2; i++)
-{
-if (i*i == n) /* square found */
-return (i);
-}
 
-/* if we haven't found a square, recurse with n-1 */
-int sqrt = _sqrt_recursion(n - 1);
-if (sqrt == -1) /* no square found */
+int _sqrt_recursion(int n)
+
+{
+if (n < 0)
+
 return (-1);
-else /* found square */
-return (sqrt);
+
+return (actual_sqrt_recursion(n, 0));
+
+}
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ *
+ * square root of a number
+ *
+ * @n: number to calculate the sqaure root of
+ *
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ *
+ */
+
+int actual_sqrt_recursion(int n, int i)
+
+{
+
+if (i * i > n)
+
+return (-1);
+
+if (i * i == n)
+
+return (i);
+
+return (actual_sqrt_recursion(n, i + 1));
+
 }
