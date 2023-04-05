407461c0338097cb5f1a8d3f8a941fd29e0af9ee
ELHEND <alhnd683@gmail.com>
2023-04-05 08:22:28 -0700

    x

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,13 +9,13 @@
 int is_prime(int n, int i)
 {
 if (n <= 2)
-return (n == 2) ? 1 : 0;
+return ((n == 2) ?  1 : 0);
 
 if (n % i == 0)
-return 0;
+return (0);
 if (i * i > n)
-return 1;
-return is_prime(n, i + 1);
+return (1);
+return (is_prime(n, i + 1));
 }
 
 /**
@@ -27,6 +27,6 @@ return is_prime(n, i + 1);
 int is_prime_number(int n)
 {
 if (n <= 1)
-return 0;
-return is_prime(n, 2);
+return (0);
+return (is_prime(n, 2));
 }
