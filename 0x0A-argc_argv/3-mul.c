56254d3422b3e526b6b8a3916d108e1f74d90380
ELHEND <alhnd683@gmail.com>
2023-04-08 10:58:28 -0700

    cccc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,10 +10,10 @@ int _atoi(char *s)
 {
 int m, d, n, len, f, digit;
 m = 0;
-d = 0;					
+d = 0;
 n = 0;
 len = 0;
-f = 0;							
+f = 0;
 digit = 0;
 while (s[len] != '\0')
 len++;
@@ -23,18 +23,18 @@ if (s[m] == '-')
 ++d;
 if (s[m] >= '0' && s[m] <= '9')
 {
-digit = s[m] - '0';	
+digit = s[m] - '0';
 if (d % 2)
 digit = -digit;
-n = n * 10 + digit;		
-f = 1;	
-if (s[m + 1] < '0' || s[m + 1] > '9')			
+n = n * 10 + digit;
+f = 1;
+if (s[m + 1] < '0' || s[m + 1] > '9')
 break;
 f = 0;
 }
 m++;
 }
-if (f == 0)	
+if (f == 0)
 return (0);
 return (n);
 }
