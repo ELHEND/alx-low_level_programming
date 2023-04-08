f5ffbb32b72a64629f769069385edd4a25186d20
ELHEND <alhnd683@gmail.com>
2023-04-08 08:24:37 -0700

    xxxxx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -9,5 +10,6 @@
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
+
 return (0);
 }
