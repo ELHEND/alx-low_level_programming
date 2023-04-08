1f9e08924ec77040b1d2dc4e6438fc964c2b536f
ELHEND <alhnd683@gmail.com>
2023-04-08 11:38:47 -0700

    zzzzz

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "main. h" 
-int main(int argc, char *argv[]) {
-	    int sum = 0;
-	        for (int i = 1; i < argc; i++) {
-			        char *num_str = argv[i];
-				        int num = 0;
-					        int j = 0;
-						        while (num_str[j] != '\0') {
-								            if (!isdigit(num_str[j])) {
-										                    printf("Error\n");
-												                    return 1;
-														                }
-									                num = num * 10 + (num_str[j] - '0');
-											            j++;
-												            }
-							        sum += num;
-								    }
-		    printf("%d\n", sum);
-		        return 0;
+
+/**
+ * is_positive_number - checks if a string represents a positive number
+ * @s: the string to check
+ *
+ * Return: 1 if s is a positive number, 0 otherwise
+ */
+int is_positive_number(const char *s)
+{
+while (*s != '\0')
+{
+if (*s < '0' || *s > '9')
+return (0);
+s++;
 }
+return (1);
+}
+
+/**
+ * main - adds positive numbers passed as command-line arguments
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 if successful, 1 if there was an error
+ */
+int main(int argc, char **argv)
+{
+int sum = 0;
+for (int i = 1; i < argc; i++)
+{
+if (!is_positive_number(argv[i]))
+{
+printf("Error\n");
+					
+return (1);
+
+}
+
+sum += atoi(argv[i]);
+
+}
+printf("%d\n", sum);
+return (0);
+}
+
