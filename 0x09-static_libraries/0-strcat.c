cdb3730e82da7f4f8543cc7cf9828c294eda036a
ELHEND <alhnd683@gmail.com>
2023-04-09 14:47:12 -0700

    henda

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -4,6 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
+ *
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
@@ -18,16 +19,12 @@ i++;
 
 while (src[j] != '\0')
 {
-
 dest[i] = src[j];
-
 j++;
 i++;
-
 }
 
 dest[i] = '\0';
-
 
 return (dest);
 }
--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_memcpy - copies memory area
+ **_memcpy - copies memory area
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
