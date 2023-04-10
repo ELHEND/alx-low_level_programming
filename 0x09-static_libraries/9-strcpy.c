934f6924787957df311b0d6e93e792dd285926f1
ELHEND <alhnd683@gmail.com>
2023-04-10 01:45:40 -0700

    henda

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- **_strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
