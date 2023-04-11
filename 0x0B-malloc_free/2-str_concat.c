1500fb060f862b3f83d5a450730f54f1d98f8de9
ELHEND <alhnd683@gmail.com>
2023-04-11 03:39:03 -0700

    zzzz

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * *str_concat - concatenates two strings
+ *str_concat - concatenates two strings
  * @s1: string to concatenate
  * @s2: other string to concatenate
  * Return: pointer to the new string created (Success), or NULL (Error)
  */
 
-
 char *str_concat(char *s1, char *s2)
 {
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
+int len1 = s1 ? strlen(s1) : 0; // check if s1 is null
+int len2 = s2 ? strlen(s2) : 0; // check if s2 is null
+char *concat_str = malloc(sizeof(char) * (len1 + len2 + 1)); // allocate memory for the concatenated string
 
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
-
-char *result = malloc(len1 + len2 + 1);
-if (result == NULL)
-{
+if (!concat_str) // check if memory allocation was successful
 return (NULL);
-}
 
-memcpy(result, s1, len1);
-memcpy(result + len1, s2, len2 + 1);
-return (result);
+if (s1)
+strcpy(concat_str, s1); // copy s1 into concat_str
+else
+*concat_str = '\0'; // if s1 is NULL, the resulting string should be empty
+
+if (s2)
+strcpy(concat_str + len1, s2); // copy s2 into concat_str after s1
+else
+*(concat_str + len1) = '\0'; // if s2 is NULL, the resulting string remains as is
+
+return (concat_str);
 }
 
