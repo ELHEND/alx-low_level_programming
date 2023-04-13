ba3be54f68ff70deb8a5dc1907c9dd69c3ac6586
ELHEND <alhnd683@gmail.com>
2023-04-13 03:38:09 -0700

    ggggg

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ *
+ * Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int k;
+for (k = 0; k < n; k++)
+
+{
+s[k] = b;
+}
+return (s);
+}
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *o;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+
+}
+o = malloc(nmemb * size);
+if (o == NULL)
+{
+return (NULL);
+}
+_memset(p, 0, (nmemb * size));
+return (o);
+}
