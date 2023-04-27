85216ced3a86dc3e65e7eb6903d48fd8ff12947a
ELHEND <alhnd683@gmail.com>
2023-04-27 03:12:48 -0700

    xc

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,13 +4,9 @@ void first(void) __attribute__ ((constructor));
  * first - prints a sentence before the main
  * function is executed
  *
- * main - prints a sentence after the  first
  */
 void first(void)
 {
-printf("This is a different message than the original one.\n");
-}
-{
-printf("Main function executed!\n");
-return (0);
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
