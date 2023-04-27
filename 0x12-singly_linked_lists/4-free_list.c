c2bc3dc135f15b81e051bb07e055432ffd8fad46
ELHEND <alhnd683@gmail.com>
2023-04-27 02:55:16 -0700

    zx

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+list_t *T;
+while (head)
+{
+T = head->next;
+free(head->str);
+free(head);
+head = T;
+}
+}
