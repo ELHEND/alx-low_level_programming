243615af0598b9e9fa431ba05343ad5f36246fa0
ELHEND <alhnd683@gmail.com>
2023-06-15 11:00:15 -0700

    h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *  * print_dlistint - prints all the elements of a
+ *   * dlistint_t list
+ *    * @h: head of the list
+ *      * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int co;
+
+	co = 0;
+
+	if (h == NULL)
+		return (co);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		co++;
+		h = h->next;
+	}
+
+	return (co);
+}
