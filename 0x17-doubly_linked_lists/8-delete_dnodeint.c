5ebf39dbae3b82cf202a19fdb2eb2f6367f66380
ELHEND <alhnd683@gmail.com>
2023-06-15 11:43:36 -0700

    l

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  *    * @head: head of the list
  *      * @index: index of the new node
  *       * Return: 1 if it succeeded, -1 if it failed
- *        */
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h1;
