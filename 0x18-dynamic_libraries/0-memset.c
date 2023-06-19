82d5cf85e3a51e255497de357067f369345da6da
ELHEND <alhnd683@gmail.com>
2023-06-19 09:31:16 -0700

    r

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * _memset - function fill the first @n bytes of the memory area pointed
+ *   * to by @s with the constant byte @b
+ *    * @n: bytes of the memory area pointed to by @s
+ *     * @s: with the constant byte @b
+ *      * @b: memory area pointe
+ *       * Return: a pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int m = 0;
+
+	while (m < n)
+	{
+		s[m] = b;
+		m++;
+	}
+	return (s);
+}
