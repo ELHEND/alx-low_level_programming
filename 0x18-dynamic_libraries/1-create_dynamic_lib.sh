28596ab9ab8abe20f0ac23638b95281265d91706
ELHEND <alhnd683@gmail.com>
2023-06-19 13:09:40 -0700

    g

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Compile all .c files into object files
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-# Create dynamic library liball.so from object files
+gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
 gcc -shared -o liball.so *.o
-# Clean up object files
-rm *.o
+
