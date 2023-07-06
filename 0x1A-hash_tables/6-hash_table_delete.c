8b8fbac52bbcfeae7d134dc686e2d2ce3947b602
ELHEND <alhnd683@gmail.com>
2023-07-06 09:31:56 -0700

    eejiycc

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ *  * hash_table_delete - Deletes a hash table.
+ *   * @ht: A pointer to hash table.
+ *    */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int m;
+
+	for (m = 0; m < ht->size; m++)
+	{
+		if (ht->array[m] != NULL)
+		{
+			node = ht->array[m];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
