47e6fa64c80425914293fcd11ad3863f6cb7e9d2
ELHEND <alhnd683@gmail.com>
2023-07-06 09:43:45 -0700

    nb

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  *     * @size: size of the array of the hash table.
  *      * Return:  index of the key.
  *        * Description: Uses the djb2 algorithm.
- *         */
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 		return (hash_djb2(key) % size);
