8e0b890f8f8d98d3a667de24083e37ab19ddfa41
ELHEND <alhnd683@gmail.com>
2023-07-06 10:17:49 -0700

    dfdf

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,12 @@
 #include "hash_tables.h"
+
 /**
  *  * hash_table_get - Retrieve the value associated with
  *   *                  a key in a hash table.
- *    * @ht: A pointer to the hash table.
- *     * @key: key to get the value of.
- *      * Return: If  key cannot be matched - NULL.
+ *    * @ht: A pointer to hash table.
+ *     * @key: The key get the value of.
+ *      *
+ *       * Return: If the key cannot be matched - NULL.
  *        *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
