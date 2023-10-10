a2707ccee735491202bcf6b2b88bd0c369922698
ELHEND <alhnd683@gmail.com>
2023-10-09 23:24:38 -0700

    hend

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -3,16 +3,17 @@
 
 /**
  * jump_list - searches for a value in an array of
- * integers using the Jump search algorithm
+ * integers using  Jump search algorithm
  *
  * @list: input list
  * @size: size of the array
  * @value: value to search in
  * Return: index of the number
  */
+
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t index, k, m;
+	size_t index, o, m;
 	listint_t *prev;
 
 	if (list == NULL || size == 0)
@@ -20,12 +21,12 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 
 	m = (size_t)sqrt((double)size);
 	index = 0;
-	k = 0;
+	o = 0;
 
 	do {
 		prev = list;
-		k++;
-		index = k * m;
+		o++;
+		index = o * m;
 
 		while (list->next && list->index < index)
 			list = list->next;
